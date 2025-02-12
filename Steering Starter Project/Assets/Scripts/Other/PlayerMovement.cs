0f01b5b6919e6889106d53561b6dfabb8d361f55
DaeStan <stanifed@my.erau.edu>
2025-02-11 22:21:46 -0700

    PR  Complete

---
+++ Steering Starter Project/Assets/Scripts/Other/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement += Vector3.back;
+        }
+
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+}
